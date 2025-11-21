b72b0f49d25efd354cba78c8e63f41abe7920258
3la2i <alaa.ata25@gmail.com>
2025-11-21 19:59:03 +0300

    fixs

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-console.log('=======env url', import.meta.env.API_BASE_URL)
+console.log('=======env urldsd', process.env.API_BASE_URL)
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.API_BASE_URL
+    baseURL: process.env.API_BASE_URL
 })
 
 
